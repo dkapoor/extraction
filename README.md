f42db7cfb70654a0be0c82069d27a6479be5646a
Brian Amanatullah <bamanatullah@inferlink.com>
2015-02-20 11:19:48 -0800

    Initial version of Landmark Extraction code

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# extraction
-Tools to do extraction from text.
+# Landmark Extraction
+
+## Running
+No third party libraries are necessary. Run the following from the command line
+python Landmark.py <FILE_TO_EXTRACT_FROM> <RULES_FILE>
+
+### Examples
+```
+python src/extraction/Landmark.py sample/jair/jair1.txt sample/jair/jair_rules.txt
+{
+  "abstract": "Call control features (e.g., call-divert, voice-mail) are primitive options to which users can subscribe off-line to personalise their  service. The configuration of a feature subscription involves choosing and sequencing features from a catalogue and is subject to  constraints that prevent undesirable feature interactions at run-time. When the subscription requested by a user is inconsistent, one  problem is to find an optimal relaxation,  which is a generalisation of the feedback vertex  set problem on directed graphs, and thus it is an NP-hard task. We present several constraint programming formulations of the problem. We also present formulations using partial  weighted maximum Boolean satisfiability and mixed integer linear programming.  We study all these formulations by experimentally comparing them  on a variety of randomly generated instances of the feature subscription problem.",
+  "authors": [
+    "Lesaint, D.",
+    "Mehta, D.",
+    "O'Sullivan, B.",
+    "Quesada, L.",
+    "Wilson, N."
+  ],
+  "pages": "271-305",
+  "title": "Developing Approaches  for Solving a Telecommunications Feature Subscription Problem",
+  "volume": "2010"
+}
+```
+
+```
+python src/extraction/Landmark.py sample/jair/jair2.txt sample/jair/jair_rules.txt
+{
+  "abstract": "Value iteration is a powerful yet inefficient algorithm for Markov decision processes (MDPs) because it puts the majority of its effort into backing up the entire state space, which turns out to be unnecessary in many cases. In order to overcome this problem, many approaches have been proposed. Among them, ILAO* and variants of RTDP are state-of-the-art ones. These methods use reachability analysis and heuristic search to avoid some unnecessary backups. However, none of these approaches build the graphical structure of the state transitions in a pre-processing step or use the structural information to systematically decompose a problem, whereby generating an intelligent backup sequence of the state space. In this paper, we present two optimal MDP algorithms. The first algorithm,  topological value iteration (TVI), detects the structure of MDPs and backs up states based on topological sequences. It (1) divides an MDP into strongly-connected components (SCCs), and (2) solves these components sequentially. TVI outperforms VI and other state-of-the-art algorithms vastly when an MDP has multiple, close-to-equal-sized SCCs. The second algorithm,  focused  topological value iteration (FTVI), is an extension of TVI. FTVI restricts its attention to connected components that are relevant for solving the MDP. Specifically, it uses a small amount of heuristic search to eliminate provably sub-optimal actions; this pruning allows FTVI to find smaller connected components, thus running faster.  We demonstrate that FTVI outperforms TVI by an order of magnitude, averaged across several domains. Surprisingly, FTVI also significantly outperforms popular `heuristically-informed' MDP algorithms such as ILAO*, LRTDP, BRTDP and Bayesian-RTDP in many domains, sometimes by as much as two orders of magnitude. Finally, we characterize the type of domains where FTVI excels --- suggesting a way to an informed choice of solver.",
+  "authors": [
+    "Dai, P.",
+    ", Mausam",
+    "Weld, D. S.",
+    "Goldsmith, J."
+  ],
+  "pages": "181-209",
+  "title": "Topological Value Iteration Algorithms",
+  "volume": "2011"
+}
+```
+
+## CREATING RULES
+There are two types of rules currently that can be used to extract information from text. They are highlighted below:
+
+### RegexRule - Used to extract one piece of content from the text
+```
+{
+    "name": "title",
+    "rule_type": "RegexRule",
+    "begin_regex": "<meta name=\"citation_title\" content=\"",
+    "end_regex": "\">"
+}
+```
+* name: the name of this rule
+* rule_type: RegexRule
+* begin_regex: The quote escaped regular expression to get to the beginning of where you would like to extract.
+* end_regex: The quote escaped regular expression to get to the end of where you would like to extract (starting from the end of begin_regex).
+
+### RegexIterationRule - Used to extract a list of content from the text
+```
+{
+    "name": "authors",
+    "rule_type": "RegexIterationRule",
+    "begin_regex": "<meta",
+    "end_regex": "</div>",
+    "iter_begin_regex": "citation_author\" content=\"",
+    "iter_end_regex": "\">"
+}
+```
+* name: the name of this rule
+* rule_type: RegexRule
+* begin_regex: The quote escaped regular expression to get to the beginning of where you would like to extract.
+* end_regex: The quote escaped regular expression to get to the end of where you would like to extract (starting from the end of begin_regex).
+* iter_begin_regex: The quote escaped regular expression for the beginning of EACH item to be repeated.
+* iter_end_regex: The quote escaped regular expression for the end EACH item to be repeated (starting from the end of each iter_begin_regex).
+
---
+++ src/extraction/Landmark.py
@@ -0,0 +1,175 @@
+import sys
+import getopt
+import re, json
+import abc
+import codecs
+import cgi
+
+def loadRule(rule_json_object):
+    """ Method to load the rules - when adding a new rule it must be added to the if statement within this method. """
+    name = rule_json_object['name']
+    rule_type = rule_json_object['rule_type']
+    sub_rules = []
+    if 'sub_rules' in rule_json_object:
+        sub_rules = rule_json_object['sub_rules']
+    
+    """ This is where we add our new type """
+    if rule_type == 'RegexRule':
+        begin_regex = rule_json_object['begin_regex']
+        end_regex = rule_json_object['end_regex']
+        rule = RegexRule(name, begin_regex, end_regex, sub_rules)
+    if rule_type == 'RegexIterationRule':
+        begin_regex = rule_json_object['begin_regex']
+        end_regex = rule_json_object['end_regex']
+        iter_begin_regex = rule_json_object['iter_begin_regex']
+        iter_end_regex = rule_json_object['iter_end_regex']
+        rule = RegexIterationRule(name, begin_regex, end_regex, iter_begin_regex, iter_end_regex, sub_rules)
+        
+    return rule
+
+class Rule:
+    """ Base abstract class for all rules """
+    __metaclass__ = abc.ABCMeta
+    
+    @abc.abstractmethod
+    def apply(self, page_string):
+        """ Method to apply this rule. The output is a JSON object of the extraction"""
+        
+    @abc.abstractmethod
+    def toolTip(self):
+        """ Method that produces an HTML tooltop for this rule """
+    
+    def __init__(self, name, sub_rules = None):
+        self.name = name
+        self.sub_rules = []
+        
+        if sub_rules:
+            self.sub_rules = RuleSet(sub_rules)
+
+class RegexRule(Rule):
+    """ Rule to apply a begin set and single end regex and return only ONE value """
+    def apply(self, page_string):
+        value = self.extract(page_string)
+        
+        if self.sub_rules:
+            value = self.sub_rules.extract(value)
+        
+        return value
+    
+    def extract(self, page_string):
+        try:
+            begin_match = self.begin_rule.search(page_string)
+            end_match = self.end_rule.search(page_string[begin_match.end():])
+            value = page_string[begin_match.end():begin_match.end()+end_match.start()]
+        except:
+            value = ''
+        return value
+    
+    def toolTip(self):
+        return 'BEGIN RULE: ' + cgi.escape(self.begin_rule.pattern) + '<hr>END RULE: ' + cgi.escape(self.end_rule.pattern)
+    
+    def __init__(self, name, begin_regex, end_regex, sub_rules = None):
+        Rule.__init__(self, name, sub_rules)
+        self.begin_rule = re.compile(begin_regex, re.S)
+        self.end_rule = re.compile(end_regex, re.S)
+        
+class RegexIterationRule(RegexRule):
+    """ Rule to apply a begin set and single end regex and return all values """
+     
+    def apply(self, page_string):
+        start_page_string = self.extract(page_string)
+        
+        values = []
+        start_index = 0
+        while start_index < len(start_page_string):
+            try:
+                begin_match = self.iter_begin_rule.search(start_page_string[start_index:])
+                end_match = self.iter_end_rule.search(start_page_string[start_index+begin_match.end():])
+                value = start_page_string[start_index+begin_match.end():start_index+begin_match.end()+end_match.start()]
+                start_index = start_index+begin_match.start()+end_match.end()
+                values.append(value)
+            except:
+                start_index = len(start_page_string)
+         
+        if self.sub_rules:
+            sub_values = []
+            for value in values:
+                sub_extraction = self.sub_rules.extract(value)
+                sub_values.append(sub_extraction)
+            values = sub_values
+             
+        return values
+    
+    def __init__(self, name, begin_regex, end_regex, iter_begin_regex, iter_end_regex, sub_rules = None):
+        RegexRule.__init__(self, name, begin_regex, end_regex, sub_rules)
+        self.iter_begin_rule = re.compile(iter_begin_regex, re.S)
+        self.iter_end_rule = re.compile(iter_end_regex, re.S)
+
+class RuleSet:
+    """A set of rules that is built from a JSON Object or JSON Array"""
+    def extract(self, page_str):
+        extraction_object = {}
+        for rule in self.rules:
+            extraction_object[rule.name] = rule.apply(page_str);
+        
+        return extraction_object
+    
+    def names(self):
+        names = []
+        for rule in self.rules:
+            names.append(rule.name)
+        return names
+    
+    def get(self, rule_name):
+        for rule in self.rules:
+            if rule.name == rule_name:
+                return rule
+        return None
+    
+    def __init__(self, json_object):
+        self.rules = []
+        rule_list = []
+        if isinstance(json_object, dict):
+            #this is a single rule so add it to the list
+            rule_list.append(json_object)
+        else:
+            rule_list = json_object
+            
+        for rule_json in rule_list:
+            self.rules.append(loadRule(rule_json))
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "h", ["help"])
+        except getopt.error, msg:
+            raise Usage(msg)
+        
+        #read the page from arg0
+        page_file_str = args[0]
+        with codecs.open(page_file_str, "r", "utf-8") as myfile:
+            page_str = myfile.read().encode('utf-8')
+
+        #read the rules from arg1
+        json_file_str = args[1]
+        with codecs.open(json_file_str, "r", "utf-8") as myfile:
+            json_str = myfile.read().encode('utf-8')
+        
+        json_object = json.loads(json_str)
+        rules = RuleSet(json_object)
+        extraction_list = rules.extract(page_str)
+        print json.dumps(extraction_list, sort_keys=True, indent=2, separators=(',', ': '))
+        
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "for help use --help"
+        return 2
+
+if __name__ == "__main__":
+    sys.exit(main())
